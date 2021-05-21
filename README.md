89064ba8dfb1c8b3cbdb986ca80ede395183d8dd
Armstrong <armstrongsouljah@gmail.com>
2021-05-21 10:42:21 +0300

    Update Retrieve and Delete

--- README.md
+++ README.md
@@ -23,3 +23,23 @@ Request methods:
 - Views handle requests from clients(browser, mobile app, standalone desktop apps, tv boxes, watches) === Devices accessing the API
 - Controller handles the business logic (full brains of the api). When a request is sent, the controller calls on the model to do something.
   A controller ends up being a middle man between the view and the model
+
+
+  CRUD (operations) and HTTP request equivalents 
+  - C => Create to create or POST something
+  - R => READ , GET something
+  - U => Update something, (PUT, PATCH)
+  - D => Delete , DELETE something from the server.
+
+
+  #### How to update
+  - Find the item
+  - Make the change
+  - Save the change if the item exists
+  - If the item does'nt exist, notify the user
+
+  ## how to delete
+  - check if there are items
+  - find a specific item
+  - delete the item 
+  - show a message or return the that doesn't have that deleted item
--- models/Review.js
+++ models/Review.js
@@ -1,14 +1,62 @@
-const reviews = []; // temporary database / store
+let reviews = []; // temporary database / store
 
 class Review {
-    constructor() {}; // only add params idf you intend to initialize with data
+    constructor() {
+        this.name = 'name'
+    }; // only add params idf you intend to initialize with data
     static  fetchReviews() {
         return reviews.length ? reviews: 'No reviews have been added';
     };
     static addReview (review) {
         return reviews.push(review);
     }
+    static getSingleReview(id) {
+        let review;
+        
+       if (reviews.length) {
+          review = reviews.find(review => review.pk == id);
+        }
+
+       if(Object.keys(review).length) {
+           return {review}
+       }
+       return {message: 'Review not found'};
+       
+    }
+
+    static updateReview(id, data) {
+        let review;
+        if (reviews.length) {
+            
+        } else {
+            return {message: "No reviews to update"}
+        }
+        if(review) {
+            let updatedReview = {...review, message: data}
+            let position = reviews.findIndex(review =>review.pk == id);
+            reviews[position] = updatedReview;
+            return {review : reviews[position]}
+        } else {
+            return {message: 'review not found'}
+        }
+    }
+
+    static removeReview(id) {
+        let review;
+        if (reviews.length) {
+            review = reviews.find(review => review.pk == id);
+            if(!review) {
+                return {message: 'review does not exist'}
+            } else {
+            reviews = reviews.filter(review => review.pk !== id);
+            return {message: 'Review removed successfully'}
+            }
+        } else {
+            return { message: "No reviews to delete"}
+        }
+    }
 };
+
 
 module.exports = {
     Review,
